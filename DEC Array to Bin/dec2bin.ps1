3a4989623599b7a194eb2fa77510761d4def646a
ryn <29420409+rynho@users.noreply.github.com>
2024-02-04 12:53:50 -0500

    Add files via upload

--- DEC Array to Bin/dec2bin.ps1
+++ DEC Array to Bin/dec2bin.ps1
@@ -1,6 +1,8 @@
 $hex = Get-Content -Path $args[0] -Raw
 
-# split the input string using ',' as delimiter, and ignor space and line break;
+# split the input string using ',' as delimiter, and trims spaces and line breaker;
+# $hex may contain decimal (no prefix, e.g., '10') or hexadecimal (prefixed with '0x', e.g., '0x0A') representing a byte.
+
 # casting the result to [byte[]] then recognizes this hex format directly.
 
 [byte[]]$bytes = ($hex -split ',').Trim()
